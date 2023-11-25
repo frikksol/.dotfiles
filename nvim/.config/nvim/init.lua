fc39531fbd2990e8a23bb4908818329aa9e61b2e
Frikk Herding <frikksol@gmail.com>
2023-11-25 15:17:10 +0100

    Vim move lazy setup to its own file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,4 @@
 --[[
-
 =====================================================================
 ========================= Frikks Nvim Config ========================
 ========================= Based on kickstart ========================
@@ -12,29 +11,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Install `lazy.nvim` plugin managers
--- https://github.com/folke/lazy.nvim
---`:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Configure plugins
-require('lazy').setup({
-  -- Import custom plugins in lua/custom/plugins
-  { import = 'custom.plugins' },
-}, {})
-
 -- Run different setup scripts
+require('lazy-setup')
 require('highlight-on-yank-setup')
 require('telescope-setup')
 require('treesitter-setup')
