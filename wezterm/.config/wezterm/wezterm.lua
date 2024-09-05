45c7f4493c8d540860a7b666deb95cbfd54e94e2
Frikk Herding <frikksol@gmail.com>
2024-09-05 09:39:19 +0200

    Update wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,7 +9,16 @@ config = {
   window_decorations = "RESIZE",
   default_cursor_style = "BlinkingBar",
   color_scheme = "Catppuccin Mocha",
-  font = wezterm.font("Jetbrains Mono", { weight = "Bold" })
+  font = wezterm.font("Jetbrains Mono", { weight = "Bold" }),
+
+  -- Workaround for keys that do not work for Norwegian keyboard
+  send_composed_key_when_left_alt_is_pressed = true,
+  keys = {
+    -- in "phys:7" means "the key in the same physical position as 7 in an ANSI standard US keyboard"
+    {key="phys:7", mods="ALT", action="DisableDefaultAssignment"},
+    {key="phys:8", mods="ALT", action="DisableDefaultAssignment"},
+    {key="phys:9", mods="ALT", action="DisableDefaultAssignment"},
+  }
 }
 
 return config
