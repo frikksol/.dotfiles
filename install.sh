5e278a2d7f8ae09cc6634af4e0cc2c280bb4c074
Frikk Herding <frikksol@gmail.com>
2024-09-05 09:39:36 +0200

    Update isntall.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,38 @@
 echo "Installing packages..."
 
 # Terminal Tools
-brew install zsh starship exa bat ripgrep fzf zoxide kitty zellij 1password-cli eza htop mdcat neovim stow the_silver_searcher tree wget wezterm
+brew install \ 
+  zsh \ 
+  starship \ # Terminal prompt
+  bat \ # Improved cat
+  ripgrep \ # Searching
+  fzf \ # Fuzzy finding for everything
+  zoxide \ # Improved cd
+  1password-cli \ # CLI for 1Password
+  eza \ # Improved ls
+  htop \ # List process info
+  mdcat \ # Markdown Reader
+  neovim \ # Vim replacement
+  stow \ # Dotfile management
+  the_silver_searcher \ # Searching, simpler that ripgrep, but super useful
+  tree \ # List folder tree structure, not installed by default on macs for some reason
+  wget \ 
+  wezterm # Awesome terminal emulator
 
 # GUI Applications
-brew install firefox alfred alt-tab amethyst bruno dozer linearmouse spotify vlc
+brew install \
+  firefox \ # I actually use zen-browser, but its not in brew yet
+  raycast \ # App launcher
+  alt-tab \ # Enable alt-tab on mac
+  bruno \ # Postman replacement
+  dozer \ # Group things in the status bar
+  linearmouse \ # Makes mouse linear on mac
+  spotify \
+  aerospace \ # Tiling window manager on mac
+  darktable \ # Photo editing
+  figma \
+  ticktick \ # Todo
+  vlc
 
 # Fonts
 brew install font-fira-mono-nerd-font font-hack-nerd-font font-jetbrains-mono-nerd-font
