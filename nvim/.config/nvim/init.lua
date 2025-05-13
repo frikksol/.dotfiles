468d2c1b643dcdda1d028607b79e8e1d1249b9b1
Frikk Herding <frikksol@gmail.com>
2025-05-13 21:41:26 +0200

    Add nvim-harpoon for file switching

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1028,6 +1028,67 @@ require('lazy').setup({
     },
   },
 
+  -- Harpoon for blazing fast file switching
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('harpoon').setup {}
+    end,
+    keys = {
+      {
+        '<leader>ha',
+        function()
+          require('harpoon'):list():add()
+        end,
+        desc = 'Add file to Harpoon',
+      },
+      {
+        '<leader>h<leader>',
+        function()
+          require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())
+        end,
+        desc = 'Toggle Harpoon Menu',
+      },
+      {
+        '<leader>hh',
+        function()
+          require('harpoon.ui').ui:toggle_quick_menu()
+        end,
+        desc = 'Toggle Harpoon Menu',
+      },
+      {
+        '<leader>h1',
+        function()
+          require('harpoon'):list():select(1)
+        end,
+        desc = 'Switch to Harpoon File 1',
+      },
+      {
+        '<leader>h2',
+        function()
+          require('harpoon'):list():select(2)
+        end,
+        desc = 'Switch to Harpoon File 2',
+      },
+      {
+        '<leader>h3',
+        function()
+          require('harpoon'):list():select(3)
+        end,
+        desc = 'Switch to Harpoon File 3',
+      },
+      {
+        '<leader>h4',
+        function()
+          require('harpoon'):list():select(4)
+        end,
+        desc = 'Switch to Harpoon File 4',
+      },
+    },
+  },
+
   -- Add shortcut to show all the shortcuts
   vim.keymap.set('n', '<leader>?', "<cmd>lua require('which-key').show()<CR>", { desc = 'Show Shortcuts' }),
 
