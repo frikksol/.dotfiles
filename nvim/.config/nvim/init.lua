499dc2c5dabf390e82004224004e7fc60b8a178e
Frikk Herding <frikksol@gmail.com>
2025-05-13 15:18:26 +0200

    Update toggle and test shortcuts

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -349,8 +349,10 @@ require('lazy').setup({
       -- Document existing key chains
       spec = {
         { '<leader>s', group = '[S]earch' },
-        { '<leader>t', group = '[T]oggle and [T]est' },
+        { '<leader>t', group = '[T]est' },
+        { '<leader>g', group = 'To[g]gle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>b', group = 'De[b]ug' },
       },
     },
   },
@@ -623,9 +625,9 @@ require('lazy').setup({
           --
           -- This may be unwanted, since they displace some of your code
           if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-            map('<leader>th', function()
+            map('<leader>gh', function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-            end, '[T]oggle Inlay [H]ints')
+            end, 'Toggle Inlay [H]ints')
           end
         end,
       })
