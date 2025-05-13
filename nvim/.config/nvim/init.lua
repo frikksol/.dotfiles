18a1f304d04145064948471cfa36808123c08e10
Frikk Herding <frikksol@gmail.com>
2025-05-13 21:41:03 +0200

    Add nvim-oil for file management

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1012,6 +1012,21 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>ts', "<cmd>lua require('neotest').summary.toggle()<CR>", { desc = 'Toggle Test [S]ummary' })
     end,
   },
+  -- Oil file manager
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    keys = {
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
+    },
+  },
 
   -- Add shortcut to show all the shortcuts
   vim.keymap.set('n', '<leader>?', "<cmd>lua require('which-key').show()<CR>", { desc = 'Show Shortcuts' }),
