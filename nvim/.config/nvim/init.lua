215302b9be7991e6b9768a87fc9bce9784f2e937
Frikk Herding <frikksol@gmail.com>
2025-05-13 09:53:39 +0200

    Add copilot

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -248,6 +248,7 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  'github/copilot.vim',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
