2fc5e4ebebb14930f3cbf3549e9cd2614171e7d5
Frikk Herding <frikksol@gmail.com>
2025-05-14 21:03:11 +0200

    Add autopairs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -913,6 +913,13 @@ require('lazy').setup({
       vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+  },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
