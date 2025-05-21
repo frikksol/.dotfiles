169ea861a1d2661069be4256b0ccf800626a829f
Frikk Herding <frikksol@gmail.com>
2025-05-21 12:29:21 +0200

    nvim: show hidden files in telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -52,7 +52,12 @@ return { -- Fuzzy Finder (files, lsp, etc)
       --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
       --   },
       -- },
-      -- pickers = {}
+      pickers = {
+        find_files = {
+          -- `hidden = true` will search hidden files
+          hidden = true,
+        },
+      },
       extensions = {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
