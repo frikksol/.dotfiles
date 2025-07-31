f1e23a5d3b899e032c58c9ce5f89fb7056a14ee9
Frikk Herding <frikksol@gmail.com>
2025-07-31 11:17:41 +0200

    nvim: Add toggleable terminal

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -124,6 +124,35 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Toggle terminal
+local term_buf = nil
+local term_win = nil
+
+function TermToggle(height)
+  if term_win and vim.api.nvim_win_is_valid(term_win) then
+    vim.cmd 'hide'
+  else
+    vim.cmd 'botright new'
+    local new_buf = vim.api.nvim_get_current_buf()
+    vim.cmd('resize ' .. height)
+    if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
+      vim.cmd('buffer ' .. term_buf) -- go to terminal buffer
+      vim.cmd('bd ' .. new_buf) -- cleanup new buffer
+    else
+      vim.cmd 'terminal'
+      term_buf = vim.api.nvim_get_current_buf()
+      vim.wo.number = false
+      vim.wo.relativenumber = false
+      vim.wo.signcolumn = 'no'
+    end
+    vim.cmd 'startinsert!'
+    term_win = vim.api.nvim_get_current_win()
+  end
+end
+
+vim.keymap.set('n', '<C-t>', ':lua TermToggle(20)<CR>', { noremap = true, silent = true })
+vim.keymap.set('t', '<C-t>', '<C-\\><C-n>:lua TermToggle(20)<CR>', { noremap = true, silent = true })
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
